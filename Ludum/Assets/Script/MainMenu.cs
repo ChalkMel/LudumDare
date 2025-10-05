cd8d11a149ad68cf1487030a2007438ef0a3e7ec
ChalkMel <melnikvv24@st.ithub.ru>
2025-10-05 12:26:15 +0300

    MainMenu
    Scene: Menu
    Play: switch to prototype scene
    Settings: empty, return button
    Ext menu

---
+++ Ludum/Assets/Script/MainMenu.cs
@@ -0,0 +1,48 @@
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject settingsMenu;
+    [SerializeField] private GameObject exitMenu;
+    [SerializeField] private Button exitMenuYesBut;
+    [SerializeField] private Button exitMenuNoBut;
+    [SerializeField] private Button exitBut;
+    [SerializeField] private Button settingsBut;
+    [SerializeField] private Button settingsMenuNoBut;
+    [SerializeField] private Button playBut;
+    private void Awake()
+    {
+        
+        exitBut.onClick.AddListener(ExitGameMenu);
+        settingsBut.onClick.AddListener(SettingsMenu);
+        playBut.onClick.AddListener(StartGame);
+    }
+    void ExitGameMenu()
+    {
+        exitMenu.SetActive(true);
+        exitMenuYesBut.onClick.AddListener(ExitGame);
+        exitMenuNoBut.onClick.AddListener(Return);
+    }
+    void ExitGame()
+    {
+        Application.Quit();
+    }
+    void Return()
+    {
+        exitMenu.SetActive(false);
+        settingsMenu.SetActive(false);
+    }
+    void SettingsMenu()
+    {
+        settingsMenu.SetActive(true);
+        settingsMenuNoBut.onClick.AddListener(Return);
+    }
+    void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
